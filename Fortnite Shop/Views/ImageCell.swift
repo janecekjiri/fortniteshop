58192eaeb1c36e42623304217aecf4eec97be362
jirijanecek <j.janecek@altairsoftware.cz>
2020-12-31 15:27:44 +0100

    Added all missing rarities

--- Fortnite Shop/Views/ImageCell.swift
+++ Fortnite Shop/Views/ImageCell.swift
@@ -81,6 +81,36 @@ class ImageCell: UICollectionViewCell {
         case .legendary:
             itemImageView.image = UIImage(named: "legendary")
             itemImageView.layer.borderColor = UIColor(red: 233, green: 141, blue: 75).cgColor
+        case .darkSeries:
+            itemImageView.image = UIImage(named: "darkSeries")
+            itemImageView.layer.borderColor = UIColor(red: 255, green: 66, blue: 231).cgColor
+        case .frozenSeries:
+            itemImageView.image = UIImage(named: "frozenSeries")
+            itemImageView.layer.borderColor = UIColor(red: 170, green: 208, blue: 238).cgColor
+        case .shadowSeries:
+            itemImageView.image = UIImage(named: "shadowSeries")
+            itemImageView.layer.borderColor = UIColor(red: 110, green: 110, blue: 110).cgColor
+        case .slurpSeries:
+            itemImageView.image = UIImage(named: "slurpSeries")
+            itemImageView.layer.borderColor = UIColor(red: 0, green: 139, blue: 222).cgColor
+        case .lavaSeries:
+            itemImageView.image = UIImage(named: "lavaSeries")
+            itemImageView.layer.borderColor = UIColor(red: 160, green: 48, blue: 53).cgColor
+        case .iconSeries:
+            itemImageView.image = UIImage(named: "iconSeries")
+            itemImageView.layer.borderColor = UIColor(red: 64, green: 184, blue: 199).cgColor
+        case .marvel:
+            itemImageView.image = UIImage(named: "marvel")
+            itemImageView.layer.borderColor = UIColor(red: 239, green: 53, blue: 55).cgColor
+        case .dcSeries:
+            itemImageView.image = UIImage(named: "dcSeries")
+            itemImageView.layer.borderColor = UIColor(red: 96, green: 148, blue: 206).cgColor
+        case .starWarsSeries:
+            itemImageView.image = UIImage(named: "starWarsSeries")
+            itemImageView.layer.borderColor = UIColor(red: 76, green: 76, blue: 76).cgColor
+        case .gamingLegendsSeries:
+            itemImageView.image = UIImage(named: "gamingLegendsSeries")
+            itemImageView.layer.borderColor = UIColor(red: 127, green: 120, blue: 249).cgColor
         default:
             itemImageView.image = UIImage(named: "common")
             itemImageView.layer.borderColor = UIColor(red: 177, green: 177, blue: 177).cgColor
