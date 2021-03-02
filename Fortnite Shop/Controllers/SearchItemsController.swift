4cd6140186f8a6ef61d65b403e9acafab04b9c78
jirijanecek <jiri.janecek@yahoo.com>
2021-03-02 10:00:46 +0100

    Got rid of empty spaces

--- Fortnite Shop/Controllers/SearchItemsController.swift
+++ Fortnite Shop/Controllers/SearchItemsController.swift
@@ -23,11 +23,11 @@ class SearchItemsController: UICollectionViewController {
     init() {
         super.init(collectionViewLayout: UICollectionViewFlowLayout())
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpSearchController()
